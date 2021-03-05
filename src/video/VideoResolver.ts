27fc1b920b698c9eead4c6b03604b3cabae688ae
luiscostalafc <luiscostalafc@gmail.com>
2021-03-05 13:57:47 -0300

    feat: finished based project

---
+++ src/video/VideoResolver.ts
@@ -0,0 +1,31 @@
+import { Resolver, Query, InputType, Field, Mutation, Arg } from 'type-graphql';
+import VideoSchema from '../model/VideoSchema';
+import Video from './Video'
+
+@InputType()
+class VideoInput {
+  @Field()
+  description: string;
+  @Field()
+  name: string;
+  @Field()
+  category: string;
+}
+
+@Resolver(Video)
+class VideoResolver {
+@Query(() => [Video])
+async videos() {
+  const videos = await VideoSchema.find();
+  return videos
+}
+
+@Mutation(() => Video)
+async createVideos(@Arg("videoInput") videoInput: VideoInput) {
+  const video = await VideoSchema.create(videoInput)
+  return video;
+}
+
+}
+
+export default VideoResolver;
